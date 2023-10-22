3c2c30b3433b3b60f9608d1199552819eef74ed0
guilhelp <guilherme.paula.dev@gmail.com>
2023-10-22 16:53:32 -0300

    migrations

---
+++ app/Models/Site_contato.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Site_contato extends Model
+{
+    use HasFactory;
+}
